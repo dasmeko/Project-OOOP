5d4d4fe465658d284766e96cd4b8bed94b212f82
dasmeko <dashadadaday@gmail.com>
2023-10-09 22:51:24 +0300

    Initial Commit

---
+++ Project-OOOP/Project_OOOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_OOOPApp.swift
+//  Project-OOOP
+//
+//  Created by я даша on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project_OOOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
