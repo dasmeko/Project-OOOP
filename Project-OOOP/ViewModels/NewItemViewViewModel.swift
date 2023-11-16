6b86277bb761d35cff9b05d86880b1b451dcfe02
dasmeko <dashadadaday@gmail.com>
2023-11-16 21:16:13 +0200

    added getting users id and saving model

--- Project-OOOP/ViewModels/NewItemViewViewModel.swift
+++ Project-OOOP/ViewModels/NewItemViewViewModel.swift
@@ -6,7 +6,8 @@
 //
 
 import Foundation
-
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
@@ -16,6 +17,25 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
+        // get user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // create model
+        
+        
+        // save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document("123")
+            .setData([:])
         
     }
     
