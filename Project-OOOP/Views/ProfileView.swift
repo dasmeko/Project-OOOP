d142e12304b010069ba1d94c0bc0ec17eddb6df6
dasmeko <dashadadaday@gmail.com>
2023-11-20 14:17:51 +0200

    made profile  view look better

--- Project-OOOP/Views/ProfileView.swift
+++ Project-OOOP/Views/ProfileView.swift
@@ -14,43 +14,51 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                // avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color.blue)
-                    .frame(width: 125, height: 125)
-                    .padding()
-                // info
-                VStack(alignment: .leading) {
-                    
-                    HStack {
-                        Text("Ім'я: ").bold()
-                        Text("Дар'я Бабенко")
-                    }.padding()
-                    
-                    HStack {
-                        Text("Пошта: ").bold()
-                        Text("dashadadaday@gmail.com")
+                if let user = viewModel.user {
+                    // avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    // info
+                    VStack(alignment: .leading) {
+                        
+                        HStack {
+                            Text("Ім'я: ").bold()
+                            Text(user.name)
+                        }.padding()
+                        
+                        HStack {
+                            Text("Пошта: ").bold()
+                            Text(user.email)
+                        }
+                        .padding()
+                        HStack {
+                            Text("Дата реєстрації: ").bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .numeric, time: .shortened))")
+                        }.padding()
+                        
                     }
                     .padding()
-                    HStack {
-                        Text("Дата реєстрації: ").bold()
-                        Text("10.10.2023")
-                    }.padding()
+                    // sign out
                     
+                    Button("Вийти") {
+                        viewModel.logOut()
+                    }.tint(Color.red)
+                        .padding()
+                        
+                    Spacer()
+           } else {
+                    Text("Завантаження профілю...")
                 }
-                .padding()
-                // sign out
-                
-                Button("Вийти") {
-                    viewModel.logOut()
-                }.tint(Color.red)
-                    .padding()
-                    
             }
             .navigationTitle("Профіль")
             
+        }
+        .onAppear {
+            viewModel.fetchUser()
         }
         
     }
